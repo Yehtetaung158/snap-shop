9a81c27bc795873cc4c32f0cf55bb9d18214be64
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-04-29 03:09:10 +0630

    apiVersion-fixed

--- server/auth.ts
+++ server/auth.ts
@@ -94,7 +94,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   events: {
     createUser: async ({ user }) => {
       const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-        apiVersion:"2025-03-31.basil"
+        apiVersion: "2025-02-24.acacia",
       });
       const customer = await stripe.customers.create({
         email: user.email!,
