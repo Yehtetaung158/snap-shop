4ff27cce2ebd05f7ab4d40a99c81fba382103be8
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-04-29 04:39:43 +0630

    6

---
+++ app/_not-found.tsx
@@ -0,0 +1,11 @@
+// app/_not-found.tsx
+export default function NotFound() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h2 className="text-4xl font-bold text-red-500">404 - Page Not Found</h2>
+        <p className="mt-4 text-gray-600">
+          ရှာဖွေနေသော စာမျက်နှာကို မတွေ့ပါ
+        </p>
+      </div>
+    );
+  }
