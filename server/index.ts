aeff3ef6cd4a445565fcc91ebecea0b4deadba35
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-05-02 07:46:48 +0630

    fix-index

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,15 @@
-import * as dotenv from "dotenv";
-dotenv.config({ path: ".env.local" });
-import * as schema from "@/server/schema";
-import { drizzle } from "drizzle-orm/neon-http";
+// import * as dotenv from "dotenv";
+// dotenv.config({ path: ".env.local" });
+// import * as schema from "@/server/schema";
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+
+// const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+// export const db = drizzle(sql, { schema });
 import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/server/schema";
 
 const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema, logger: true });
+
